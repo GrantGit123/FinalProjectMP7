b0acf808ec3499c367abf6e7f8feea31267c8e23
GrantGit123 <gaguir9@illinois.edu>
2018-04-24 19:47:11 -0500

    Imported GSON and created a Backend class(we can rename it) - ready to start coding

--- app/src/main/java/com/finalprojectmp7/Backend.java
+++ app/src/main/java/com/finalprojectmp7/Backend.java
@@ -4,5 +4,9 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 public class Backend {
+/**
+ * Need to read JSON passed into app
+ * Parse and use it like MP6
+ */
 
 }
