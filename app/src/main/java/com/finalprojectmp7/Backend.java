83124c3b323de17907b334b0586acf4b8f855d2f
GrantGit123 <gaguir9@illinois.edu>
2018-05-01 23:27:35 -0500

    UPDATED- 11:27pm

--- app/src/main/java/com/finalprojectmp7/Backend.java
+++ app/src/main/java/com/finalprojectmp7/Backend.java
@@ -1,33 +1,37 @@
 package com.finalprojectmp7;
-import android.util.Log;
-import android.widget.SearchView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+//import android.util.Log;
+//import android.widget.SearchView;
+//
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.StringRequest;
+//import com.android.volley.toolbox.Volley;
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
+//import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
 
 public class Backend  {
     private static final String TAG = "MP7:Main- Backend";
 
 
-    public static int getClose(final String json) {
+    public static float[] getClose(final String json) {
         if (json == null) {
-            return 0;
+            return null;
         }
         JsonParser parser = new JsonParser();
         JsonObject obj = parser.parse(json).getAsJsonObject();
         JsonObject dataSet = obj.getAsJsonObject("dataset");
         JsonArray data = dataSet.getAsJsonArray("data");
-        JsonArray recentDate = data.get(0).getAsJsonArray();
-        return recentDate.get(4).getAsInt();
+        float[] closingValues = new float[7];
+        for (int j = 0; j < 7; j++) {
+            closingValues[j] = data.get(j).getAsJsonArray().get(4).getAsFloat();
+        }
+        return closingValues;
     }
 }
--- app/src/main/java/com/finalprojectmp7/MainActivity.java
+++ app/src/main/java/com/finalprojectmp7/MainActivity.java
@@ -2,9 +2,9 @@ package com.finalprojectmp7;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,11 +18,10 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Arrays;
+
 
 
 /**
@@ -37,7 +36,6 @@ public final class MainActivity extends AppCompatActivity {
     private static RequestQueue requestQueue;
 
     Button button;
-    TextView text;
     SearchView searchView;
 
 
@@ -50,28 +48,84 @@ public final class MainActivity extends AppCompatActivity {
         // Set up a queue for our Volley requests
         requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main);
-
-        text = findViewById(R.id.textView);
         searchView = findViewById(R.id.searchbar);
+        button = findViewById(R.id.button);
         searchView.setOnQueryTextListener(createSearchQueryListener());
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Get Graph Button Clicked");
+                createGraph();
+            }
+        });
+    }
+     //make API calls
+    private String query;
+    private void startAPICall() {
+        try {
+            StringRequest getJson = new StringRequest(
+                    Request.Method.GET,
+                    "https://www.quandl.com/api/v3/datasets/WIKI/" + query + ".json",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(final String response) {
+                            jsonResult = response;
+                            //Log.d(TAG, Arrays.toString(Backend.getClose(jsonResult)));
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, "ERROR RESPONSE");
+                }
+            });
+            requestQueue.add(getJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+   //Searchbar listener
+    private SearchView.OnQueryTextListener createSearchQueryListener() {
+        try {
+            return new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(final String input) {
+                    query = input;
+                    startAPICall();
+                    return false;
+                }
+                @Override
+                public boolean onQueryTextChange(final String newText) {
+                    return true;
+                }
+            };
+        } catch (NullPointerException e) {
+            startAPICall();
+        }
+        return null;
+    }
+
+    public void createGraph() {
         try {
             lineChart = findViewById(R.id.LineChart);
-            //lineChart.setOnChartValueSelectedListener(MainActivity.this);
-            //lineChart.setOnChartGestureListener();
             lineChart.setDragEnabled(true);
             lineChart.setScaleEnabled(false);
 
-            ArrayList<Entry> yValues = new ArrayList<>();
+            ArrayList<Entry> values = new ArrayList<>();
 
             //put a for loop here to put in all the values for yAxis (high values within entry dates)
-            yValues.add(new Entry(0, 60f));
-            yValues.add(new Entry(1, 60f));
-            yValues.add(new Entry(2, 60f));
-            yValues.add(new Entry(3, 60f));
-            yValues.add(new Entry(5, 60f));
+            float[] closingPrices = Backend.getClose(jsonResult);
 
-            LineDataSet set1 = new LineDataSet(yValues, "Data set 1");
+
+            values.add(new Entry(1, closingPrices[6]));
+            values.add(new Entry(2, closingPrices[5]));
+            values.add(new Entry(3, closingPrices[4]));
+            values.add(new Entry(4, closingPrices[3]));
+            values.add(new Entry(5, closingPrices[2]));
+            values.add(new Entry(6, closingPrices[1]));
+            values.add(new Entry(7, closingPrices[0]));
+
+            LineDataSet set1 = new LineDataSet(values, "Data set 1");
 
             set1.setFillAlpha(110);
 
@@ -86,83 +140,6 @@ public final class MainActivity extends AppCompatActivity {
         } catch (NegativeArraySizeException e) {
             Log.d(TAG, "NEGATIVE???");
         }
-        Backend closingPrice = new Backend();
-        int help = closingPrice.getClose(jsonResult);
-        Log.d(TAG,"FUCK" + help);
-    }
-     //make API calls
-    private String query;
-    private void startAPICall() {
-        try {
-            StringRequest getJson = new StringRequest(
-                    Request.Method.GET,
-                    "https://www.quandl.com/api/v3/datasets/WIKI/" + query + ".json",
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(final String response) {
-                            Log.d(TAG, "HAI");
-                            Log.d(TAG, response);
-                            jsonResult = response;
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.w(TAG, "ERROR RESPONSE");
-                    text.setText("ERROR RESPONSE");
-                }
-            });
-            requestQueue.add(getJson);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        text.setText(jsonResult);
-    }
-
-
-   //Searchbar listener
-    private SearchView.OnQueryTextListener createSearchQueryListener() {
-        try {
-            return new SearchView.OnQueryTextListener() {
-                @Override
-                public boolean onQueryTextSubmit(final String input) {
-                    query = input;
-                    startAPICall();
-                    Log.d(TAG, "HERE");
-                    return false;
-                }
-                @Override
-                public boolean onQueryTextChange(final String newText) {
-                    return true;
-                }
-            };
-        } catch (NullPointerException e) {
-            startAPICall();
-        }
-        return null;
-    }
-
-    /*
-     * ArrayList of dates between two input dates.
-     */
-    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
-        ArrayList<String> list = new ArrayList<String>();
-        while(startDate.compareTo(endDate)<=0){
-            list.add(getDate(startDate));
-            startDate.add(Calendar.DAY_OF_MONTH,1);
-        }
-        return list;
-    }
-    //helper function to getDate
-    public String getDate(Calendar cld){
-        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
-                +cld.get(Calendar.DAY_OF_MONTH);
-        try{
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
-            curDate =  new SimpleDateFormat("yyyy/MM/dd").format(date);
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-        return curDate;
     }
 
 }
